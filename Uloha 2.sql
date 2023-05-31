c027f966430dbbb03956fc9bda23acfd42504a92
Mario Soldan <soldan.majo@gmail.com>
2023-05-31 20:45:26 +0200

    Kozmeticka uprava kodu

--- Uloha 2.sql
+++ Uloha 2.sql
@@ -2,88 +2,92 @@
 
 -- Chlieb v prvom porovnatelnom obdobi
 SELECT 
-industry_branch,
-industry_branch_code,
-average_wages,
-food_category,
-food_price,
-DATE_FORMAT(MIN(price_measured_from), '%e. %M %Y') AS p_measured_from,
-DATE_FORMAT(MIN(price_measured_to), '%e. %M %Y') AS p_measured_to,
-payroll_quarter,
-food_price_measured_quarter,
-ROUND(average_wages/food_price,1) AS affordable_amount_of_food
+	industry_branch,
+	industry_branch_code,
+	average_wages,
+	food_category,
+	food_price,
+	DATE_FORMAT(MIN(price_measured_from), '%e. %M %Y') AS p_measured_from,
+	DATE_FORMAT(MIN(price_measured_to), '%e. %M %Y') AS p_measured_to,
+	payroll_quarter,
+	food_price_measured_quarter,
+	ROUND(average_wages/food_price,1) AS affordable_amount_of_food
 FROM t_mario_soldan_project_SQL_primary_final
-WHERE food_category_code = '111301' AND food_price_measured_quarter = ( -- povodne som chcel dat iba food_price_measured_quarter = payroll_quarter avsak TO mi nezobrazovalo spravne veci, preto som TO musel vymsliet inak. Tento princip som pouzil pri vsetkych selectoch v tejto ulohe
-SELECT food_price_measured_quarter
-FROM t_mario_soldan_project_SQL_primary_final
-WHERE food_category_code = '111301'
-ORDER BY price_measured_from ASC
-LIMIT 1)
+WHERE food_category_code = '111301' 
+	AND food_price_measured_quarter = ( -- povodne som chcel dat iba food_price_measured_quarter = payroll_quarter avsak TO mi nezobrazovalo spravne veci, preto som TO musel vymsliet inak. Tento princip som pouzil pri vsetkych selectoch v tejto ulohe
+	SELECT food_price_measured_quarter
+	FROM t_mario_soldan_project_SQL_primary_final
+	WHERE food_category_code = '111301'
+	ORDER BY price_measured_from ASC
+	LIMIT 1)
 GROUP BY industry_branch_code
 ORDER BY industry_branch_code ASC;
 
 -- Mlieko v prvom porovnatelnom obdobi
 SELECT 
-industry_branch,
-industry_branch_code,
-average_wages,
-food_category,
-food_price,
-DATE_FORMAT(MIN(price_measured_from), '%e. %M %Y') AS p_measured_from,
-DATE_FORMAT(MIN(price_measured_to), '%e. %M %Y') AS p_measured_to,
-payroll_quarter,
-food_price_measured_quarter,
-ROUND(average_wages/food_price,1) AS affordable_amount_of_food
-FROM t_mario_soldan_project_SQL_primary_final
-WHERE food_category_code = '114201' AND food_price_measured_quarter = (
-SELECT food_price_measured_quarter
+	industry_branch,
+	industry_branch_code,
+	average_wages,
+	food_category,
+	food_price,
+	DATE_FORMAT(MIN(price_measured_from), '%e. %M %Y') AS p_measured_from,
+	DATE_FORMAT(MIN(price_measured_to), '%e. %M %Y') AS p_measured_to,
+	payroll_quarter,
+	food_price_measured_quarter,
+	ROUND(average_wages/food_price,1) AS affordable_amount_of_food
 FROM t_mario_soldan_project_SQL_primary_final
 WHERE food_category_code = '114201'
-ORDER BY price_measured_from ASC
-LIMIT 1)
+	AND food_price_measured_quarter = (
+	SELECT food_price_measured_quarter
+	FROM t_mario_soldan_project_SQL_primary_final
+	WHERE food_category_code = '114201'
+	ORDER BY price_measured_from ASC
+	LIMIT 1)
 GROUP BY industry_branch_code
 ORDER BY industry_branch_code ASC;
 
 -- Chlieb v poslednom porovnatelnom obdobi
 SELECT 
-industry_branch,
-industry_branch_code,
-average_wages,
-food_category,
-food_price,
-DATE_FORMAT(MAX(price_measured_from), '%e. %M %Y') AS p_measured_from,
-DATE_FORMAT(MAX(price_measured_to), '%e. %M %Y') AS p_measured_to,
-payroll_quarter,
-food_price_measured_quarter,
-ROUND(average_wages/food_price,1) AS affordable_amount_of_food
-FROM t_mario_soldan_project_SQL_primary_final
-WHERE food_category_code = '111301' AND payroll_quarter = (
-SELECT food_price_measured_quarter
+	industry_branch,
+	industry_branch_code,
+	average_wages,
+	food_category,
+	food_price,
+	DATE_FORMAT(MAX(price_measured_from), '%e. %M %Y') AS p_measured_from,
+	DATE_FORMAT(MAX(price_measured_to), '%e. %M %Y') AS p_measured_to,
+	payroll_quarter,
+	food_price_measured_quarter,
+	ROUND(average_wages/food_price,1) AS affordable_amount_of_food
 FROM t_mario_soldan_project_SQL_primary_final
 WHERE food_category_code = '111301'
-ORDER BY price_measured_from DESC
-LIMIT 1)
+	AND payroll_quarter = (
+	SELECT food_price_measured_quarter
+	FROM t_mario_soldan_project_SQL_primary_final
+	WHERE food_category_code = '111301'
+	ORDER BY price_measured_from DESC
+	LIMIT 1)
 GROUP BY industry_branch_code
 ORDER BY industry_branch_code ASC;
 
 -- Mlieko v poslednom porovnatelnom obdobi
 SELECT 
-industry_branch,
-industry_branch_code,
-average_wages,
-food_category,
-food_price,
-DATE_FORMAT(MAX(price_measured_from), '%e. %M %Y') AS p_measured_from,
-DATE_FORMAT(MAX(price_measured_to), '%e. %M %Y') AS p_measured_to,
-payroll_quarter,
-food_price_measured_quarter,
-ROUND(average_wages/food_price,1) AS affordable_amount_of_food
-FROM t_mario_soldan_project_SQL_primary_final
-WHERE food_category_code = '114201' AND  payroll_quarter = (
-SELECT food_price_measured_quarter
+	industry_branch,
+	industry_branch_code,
+	average_wages,
+	food_category,
+	food_price,
+	DATE_FORMAT(MAX(price_measured_from), '%e. %M %Y') AS p_measured_from,
+	DATE_FORMAT(MAX(price_measured_to), '%e. %M %Y') AS p_measured_to,
+	payroll_quarter,
+	food_price_measured_quarter,
+	ROUND(average_wages/food_price,1) AS affordable_amount_of_food
 FROM t_mario_soldan_project_SQL_primary_final
 WHERE food_category_code = '114201'
-ORDER BY price_measured_from DESC
-LIMIT 1)
+	AND  payroll_quarter = (
+	SELECT food_price_measured_quarter
+	FROM t_mario_soldan_project_SQL_primary_final
+	WHERE food_category_code = '114201'
+	ORDER BY price_measured_from DESC
+	LIMIT 1)
 GROUP BY industry_branch_code
 ORDER BY industry_branch_code ASC;
--- Uloha 3.sql
+++ Uloha 3.sql
@@ -4,26 +4,26 @@
 
 CREATE OR REPLACE TABLE t_mario_soldan_pomocna_tabulka_1 (
 SELECT
-DISTINCT food_category_code,
-food_category,
-food_price,
-MAX(price_measured_from) AS p_measured_date
+	DISTINCT food_category_code,
+	food_category,
+	food_price,
+	MAX(price_measured_from) AS p_measured_date
 FROM t_mario_soldan_project_SQL_primary_final
 WHERE food_price_measured_quarter = 4
 GROUP BY YEAR(price_measured_from), food_category_code);
 
 
 SELECT
-tmspt.food_category_code,
-tmspt.food_category,
-tmspt.food_price AS food_price_year,
-YEAR(tmspt.p_measured_date) AS year,
-tmspt2.food_price AS food_price_prev_year,
-YEAR(tmspt2.p_measured_date) AS prev_year,
-round((tmspt.food_price-tmspt2.food_price)/tmspt2.food_price*100,2) as price_growth
+	tmspt.food_category_code,
+	tmspt.food_category,
+	tmspt.food_price AS food_price_year,
+	YEAR(tmspt.p_measured_date) AS year,
+	tmspt2.food_price AS food_price_prev_year,
+	YEAR(tmspt2.p_measured_date) AS prev_year,
+	round((tmspt.food_price-tmspt2.food_price)/tmspt2.food_price*100,2) as price_growth
 FROM t_mario_soldan_pomocna_tabulka_1 tmspt
 JOIN t_mario_soldan_pomocna_tabulka_1 tmspt2
-ON tmspt.food_category_code = tmspt2.food_category_code
-AND YEAR(tmspt.p_measured_date) = YEAR(tmspt2.p_measured_date) + 1
-AND YEAR(tmspt.p_measured_date) < 2019
+	ON tmspt.food_category_code = tmspt2.food_category_code
+	AND YEAR(tmspt.p_measured_date) = YEAR(tmspt2.p_measured_date) + 1
+	AND YEAR(tmspt.p_measured_date) < 2019
 GROUP BY tmspt.food_category_code, tmspt.p_measured_date;
